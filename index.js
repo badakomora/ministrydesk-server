d6ccfc87dbb64a97b2ea60725e3073a59e15ed77
badakomora <badakomora06@gmail.com>
2026-01-20 20:59:00 -0800

    urls

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ const port = process.env.PORT;
 
 
 const allowedClients = [
-  'http://localhost:3000',
+  // 'http://localhost:3000',
+   'https://pagfamily.vercel.app',
 ];
 
 // Enhanced CORS configuration
